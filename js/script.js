ca25d456ed22a0ee45c147cfdf1d1c2b976f1299
Diana <Dianazare4na00@gmail.com>
2021-08-23 11:56:19 +0300

    article

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function clickContact(){
+	let contactTelegramm = document.getElementById("contTelegramm");
+	contactTelegramm.style.display = "block";
+}
+function clickTelegramm(){
+	let contactTelegramm = document.getElementById("contTelegramm");
+	contactTelegramm.style.display = "none";
+}
