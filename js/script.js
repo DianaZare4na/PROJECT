b108176045167cef98d8b994b4493a57bab16d85
Diana <Dianazare4na00@gmail.com>
2021-08-24 19:46:21 +0300

    article change

--- js/script.js
+++ js/script.js
@@ -6,3 +6,27 @@ function clickTelegramm(){
 	let contactTelegramm = document.getElementById("contTelegramm");
 	contactTelegramm.style.display = "none";
 }
+let firstarray = document.getElementById("firstarray");
+
+function firstarrayMuve(){
+	if(firstarray.style.marginTop = "239px"){
+		firstarray.style.position = "relative";
+	}
+}
+firstarrayMuve();
+
+// window.addEventListener('scroll', function() {
+// 	document.getElementById('showScroll').innerHTML = pageYOffset + 'px';
+// 	let firstarray = document.getElementById("firstarray");
+// 	firstarray.style.marginTop = "239px";
+// 	let smallarr = document.getElementById("smallarr");
+// 	smallarr.style.marginTop = "239px";
+	
+// });
+// window.addEventListener('scroll', function() {
+// 	document.getElementById('showScroll').innerHTML = pageYOffset + 'px';
+	
+// 	let smallarr = document.getElementById("smallarr");
+// 	smallarr.style.marginTop = "239px";
+	
+// });
